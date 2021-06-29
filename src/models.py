c45f67391bcfe883819e2a8c82e6e14b1e7caa3c
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-29 19:15:55 +0000

    diagramacion

--- src/models.py
+++ src/models.py
@@ -8,23 +8,76 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class People(Base):
+    __tablename__ = 'people'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Species(Base):
+    __tablename__ = 'species'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Store(Base):
+    __tablename__ = 'store'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    People = Column(Integer, ForeignKey('poeple.id'))
+    People = relationship(People)
+    Starships = Column(Integer, ForeignKey('starships.id'))
+    Starships = relationship(Starships)
+    Planets = Column(Integer, ForeignKey('planets.id'))
+    Planets = relationship(Planets)
+    Species = Column(Integer, ForeignKey('species.id'))
+    Species = relationship(Species)
+    Vehicles = Column(Integer, ForeignKey('vehicles.id'))
+    Vehicles = relationship(Vehicles)
+
+class Actions(Base):
+    __tablename__ = 'actions'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Flux(Base):
+    __tablename__ = 'flux'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    store = Column(Integer, ForeignKey('store.id'))
+    Store = relationship(Store)
+    Actions = Column(Integer, ForeignKey('actions.id'))
+    Actions = relationship(Actions)
+
 
     def to_dict(self):
         return {}
